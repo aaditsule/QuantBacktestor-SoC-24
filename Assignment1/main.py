698e27cd7d10a215b42cbd0a0508a8e23afba502
Aadit Sule <aaditsule0605@gmail.com>
2024-06-19 23:43:46 +0530

    Update main.py

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,5 +1,12 @@
-print("Hello World")
-import pandas as pd 
-list = [1,2,3,4]
-df = pd.DataFrame(list)
-print(df)
+from data_fetch import download_historical_data
+from performance import plot_closing_prices
+from datetime import datetime
+
+symbol = "RELIANCE.NS"
+start_date = "2023-06-01"
+end_date = datetime.today().strftime("%Y-%m-%d")
+timeframe = "1d"
+
+stock_data = download_historical_data(symbol, start_date, end_date, timeframe)
+
+plot_closing_prices(stock_data)
--- Assignment1/performance.py
+++ Assignment1/performance.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def plot_closing_prices(data: pd.DataFrame):
-    plt.figure(figsize=(10, 5))
     plt.plot(data.index, data['Close'], label='Closing Price')
     plt.title("Closing Price")
     plt.xlabel("Date")
