99113ad04f15167ca6c187d9d8e9ce7af7055c21
Aadit Sule <aaditsule0605@gmail.com>
2024-06-20 12:11:56 +0530

    Update main.py

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -3,10 +3,12 @@ from performance import plot_closing_prices
 from datetime import datetime
 
 symbol = "RELIANCE.NS"
-start_date = "2023-06-01"
+start_date = "2024-06-01"
 end_date = datetime.today().strftime("%Y-%m-%d")
 timeframe = "1d"
 
 stock_data = download_historical_data(symbol, start_date, end_date, timeframe)
+
+print(stock_data)
 
 plot_closing_prices(stock_data)
